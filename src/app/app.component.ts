01c36e64cfc6c2bded253d2370bc2a06f3258f04
Elysom <francardonacarrion@gmail.com>
2025-02-07 09:48:39 +0100

    Estilo fuente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { SliderfotosComponent } from "./components/sliderfotos/sliderfotos.component";
+import { CardComponent } from "./components/card/card.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SliderfotosComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SliderfotosComponent, CardComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  // Aquí puedes añadir lógica adicional si es necesario
+  
 }
